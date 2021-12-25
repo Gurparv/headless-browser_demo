36167bd54b65f06b54a4105a3fc9808f01ff2e35
Gurparv Singh <gurparv@gmail.com>
2021-12-25 12:36:34 +0530

    Add method to take screenshots and call this in both chomeHeadless and PhantomJSHeadless.

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,6 +2,9 @@ package org.example;
 
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -11,21 +14,28 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+   private WebDriver driver = null;
+
    @Test
    public void openChromeHeadless(){
       WebDriverManager.chromedriver().setup();
       ChromeOptions op = new ChromeOptions();
       op.addArguments("--headless");
-      WebDriver driver = new ChromeDriver(op);
+      driver = new ChromeDriver(op);
       driver.manage().window().maximize();
       try {
          System.out.println("Navigate to Google.com using Chrome headless browser");
          driver.get("https://google.com");
+         System.out.println("Take screenshot of page.");
+         takesScreenShot("Check Page Title.");
          System.out.println("check the page Title");
          Assert.assertEquals(driver.getTitle(), "Google");
          System.out.println("Page title = Google");
@@ -41,10 +51,12 @@ public class AppTest
    public void openPhantomJSHeadless(){
       DesiredCapabilities cp = new DesiredCapabilities();
       cp.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,System.getProperty("user.dir")+"\\phantomjs.exe");
-      WebDriver driver = new PhantomJSDriver(cp);
+      driver = new PhantomJSDriver(cp);
       try {
          System.out.println("Navigate to Google.com using Chrome headless browser");
          driver.get("https://google.com");
+         System.out.println("Take screenshot of page.");
+         takesScreenShot("Check Page Title.");
          System.out.println("check the page Title");
          Assert.assertEquals(driver.getTitle(), "Google");
          System.out.println("Page title = Google");
@@ -54,6 +66,19 @@ public class AppTest
       } finally {
          driver.quit();
       }
+   }
+
+   private void takesScreenShot(String checkName){
+      TakesScreenshot screenshot = (TakesScreenshot) driver;
+      File file = screenshot.getScreenshotAs(OutputType.FILE);
+      File Destfile= new File(System.getProperty("user.dir")+"\\screenshots\\"+
+              checkName+""+System.currentTimeMillis()+".png");
+      try{
+         FileUtils.copyFile(file,Destfile);
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+
    }
 
 }
