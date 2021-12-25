f7928461fdfa9ff895c1e206e193591f7c0da286
Gurparv Singh <gurparv@gmail.com>
2021-12-25 09:29:24 +0530

    Add dependencies in POM.xml

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,19 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+   @Test
+   public void openHeadlessBrowser(){
+      WebDriverManager.chromedriver().setup();
+      WebDriver driver = new ChromeDriver();
+   }
 }
